fc8fc42f564d54cdf25327eab1485fd3d9232c53
Nouman Arshad <noumanarshad15926@gmail.com>
2023-10-23 18:44:57 +0500

    Update page.js

--- app/app/page.js
+++ app/app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
     <>
       <Header />
       <Hero />
-      <FeaturesListicle />
-      <Footer />
+      {/* <FeaturesListicle /> */}
+      {/* <Footer /> */}
     </>
   );
 }
